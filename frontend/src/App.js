e8f319f27a29e0e436aa4c9b2498b4012f328688
Jethet <henriette.hettinga@gmail.com>
2020-12-26 12:24:18 +0100

    Clean React text and logos; add placeholder text and icon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h2>This is the personal portfolio of a CodeWoman</h2>
+
+        <img src="./images/codewomanDark.png" className="woman-icon" alt="Woman programmer" />
       </header>
     </div>
   );
