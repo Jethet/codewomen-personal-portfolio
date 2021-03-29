6ccfeaae9b33e396dd700db04ed87a48524635e4
Bips39 <physiobipasha39@gmail.com>
2021-03-29 12:04:18 +0200

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import FooterPage from './pages/footerPage/FooterPage';
 
 function App() {
+
   return (
+    <Router> 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="footer-container">
+      <Route path="/footer">
+      <FooterPage />
+      </Route>
+      </div>
+     
+      
+      
     </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/footerPage/FooterPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Footer from'../../component/footer/Footer'
+
+const FooterPage =()=>{
+    return(
+        <div>
+         <Footer />
+        </div>
+    )
+}
+export default FooterPage;
